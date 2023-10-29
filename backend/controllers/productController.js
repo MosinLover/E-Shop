2349e3df233e44be4b316f3b6294616164b268ba
Chen Jun Chi <93779005+MosinLover@users.noreply.github.com>
2023-10-29 17:03:37 -0400

    pagination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,14 +1,31 @@
 const recordsPerPage = require("../config/pagination");
-const Product = require("../models/ProductModel")
+const Product = require("../models/ProductModel");
 
 const getProducts = async (req, res, next) => {
-    try {
-        const products = await Product.find({}).sort({name: 1}).limit(recordsPerPage);
-        res.json({products});
-    } catch(error) {
-        next(error);
-    }
-}
+  try {
+    const pageNum = Number(req.query.pageNum) || 1;
+    const totalProducts = await Product.countDocuments({});
 
+    // sort
+    let sort = {};
+    const sortOption = req.query.sort || "";
+    if(sortOption) {
+        let sortOpt = sortOption.split("_");
+        sort = {[sortOpt[0]]: Number(sortOpt[1])};
+        console.log(sort);
+    }
+    const products = await Product.find({})
+      .skip(recordsPerPage * (pageNum - 1))
+      .sort({ name: 1 })
+      .limit(recordsPerPage);
+    res.json({
+      products,
+      pageNum,
+      paginationLinksNumber: Math.ceil(totalProducts / recordsPerPage),
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = getProducts;
